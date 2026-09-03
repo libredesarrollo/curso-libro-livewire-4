f9353931270ab69159aa6e578cff66447f55975c
andres <desarrollolibre.net@gmail.com>
2026-09-03 06:57:06 -0400

    proyecto actualizado

--- storage/framework/views/202879df7fa32d3379b2ba014b9871cd.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** @var \Laravel\Boost\Install\GuidelineAssist $assist */
-?>
-## Pest
-
-- This project uses Pest for testing. Create tests: ___SINGLE_BACKTICK___<?php echo e($assist->artisanCommand('make:test --pest {name}')); ?>___SINGLE_BACKTICK___.
-- Run tests: ___SINGLE_BACKTICK___<?php echo e($assist->artisanCommand('test --compact')); ?>___SINGLE_BACKTICK___ or filter: ___SINGLE_BACKTICK___<?php echo e($assist->artisanCommand('test --compact --filter=testName')); ?>___SINGLE_BACKTICK___.
-- Do NOT delete tests without approval.
-<?php /**PATH C:\Users\andre\Herd\livewirestore\storage\framework\views/e232917018da22be9b5c717021c8a6c8.blade.php ENDPATH**/ ?>
--- storage/framework/views/d8c9625d1f0183628539b830b68a6661.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes); ?> class="inline-flex items-center rounded-md bg-red-500 px-3 py-2 font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 dark:bg-red-600">
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH /Users/andrescruz/Herd/livewirestore/vendor/livewire-filemanager/filemanager/src/../resources/views/components/buttons/danger.blade.php ENDPATH**/ ?>
